430d5474c71d34e11753d24397fb2f0c0ffe5bfd
korntewin <korntewin.b@gmail.com>
2024-04-17 21:38:44 +0700

    feature: add hovering shortcut to observe variable type

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -83,6 +83,7 @@ return {
     mappings = {
       n = {
         gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
+        si = { function() vim.lsp.buf.hover() end, desc="Hover symbol inspection" },
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
         -- gD = {
         --   function() vim.lsp.buf.declaration() end,
