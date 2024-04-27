24f2ebdb6a96fd3c2f796d31c081ce1bff75ae43
korntewin <korntewin.b@gmail.com>
2024-04-27 19:17:50 +0700

    feature: switch theme to monokai-pro machine

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,34 @@
+-- return {
+--   "scottmckendry/cyberdream.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("cyberdream").setup {
+--       transparent = true,
+--       italic_comments = true,
+--       hide_fillchars = true,
+--       borderless_telescope = true,
+--       terminal_colors = true,
+--     }
+--     vim.cmd "colorscheme cyberdream"
+--   end,
+-- }
+
 return {
-  "scottmckendry/cyberdream.nvim",
+  "loctvl842/monokai-pro.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("cyberdream").setup({
-      transparent = true,
-      italic_comments = true,
-      hide_fillchars = true,
-      borderless_telescope = true,
-      terminal_colors = true,
-    })
-    vim.cmd("colorscheme cyberdream")
+    require("monokai-pro").setup {
+      filter = "machine", -- classic | octagon | pro | machine | ristretto | spectrum
+    }
+    vim.cmd "colorscheme monokai-pro"
   end,
 }
+
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function() vim.cmd "colorscheme tokyonight-night" end,
+-- }
